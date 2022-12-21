4a59825b96a4097229bcc7fcef74dc85d33ad3ff
Jawad Shani <jawad.shani@hitachi-eu.com>
2022-12-21 16:32:38 +0100

    conditonal visible component added

--- src/sum/sum.js
+++ src/sum/sum.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {ConditionalDisplay} from "../cond-display/cond-display"
 
 //React is component based.Each component is a separate concern. Think of component as simple functions in programming.
 //function components / class components
@@ -12,11 +13,31 @@ import React from "react";
 
 
 
-
 //functional component
 export function Sum(props) {
-    return (<h1>{props.a} + {props.b} = {props.a + props.b}</h1>)
+    return (
+        <div>
+            <h1>{props.a} + {props.b} = {props.a + props.b}</h1>
+            <Events />
+        </div>
+
+    )
 }
+//sub element
+function Events(props) {
+    const clickHandler = console.log; //when its just delgating
+    return (
+        <ConditionalDisplay isVisible={true}>
+        <button className="btn btn-info" onClick={clickHandler} >
+            Make an event
+        </button>
+        </ConditionalDisplay>
+    )
+}
+
+
+
+
 
 //Class syntax: this syntax has some additional features, we may need occasionally.
 
@@ -27,6 +48,8 @@ export function Sum(props) {
 export class Sum1 extends React.Component {
 
     render() {
-        return <h1>{this.props.a} + {this.props.b} = {this.props.a + this.props.b}</h1>
+        return(   
+            <h1>{this.props.a} + {this.props.b} = {this.props.a + this.props.b}</h1>           
+        )      
     }
 }
