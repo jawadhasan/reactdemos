c1d4b2ca2efb17e733f410b20c0b81aa0c2e4aa0
Jawad Shani <jawad.shani@hitachi-eu.com>
2022-12-21 13:50:18 +0100

    ajax users management

--- src/ajax-test/ajax-test.js
+++ src/ajax-test/ajax-test.js
@@ -1,14 +1,88 @@
 import React from "react";
 
-export class AjaxTest extends React.Component{    
 
-    //ctor
-    constructor(props){
-        super(props);       
+const TableHeader = (props) => {
+    let thTags = [];
+    let headers = props.tableheaders.split(' ');
+
+    for (let i = 0; i < headers.length; i++) {
+        // console.log(headers[i]);
+        thTags.push(<th key={i}>{headers[i]}</th>);
     }
 
-    AddUser(){       
-       
+    return (
+        <thead>
+            <tr>
+                {thTags}
+            </tr>
+        </thead>
+    );
+}
+const TableRow = (props) => {
+    const row = props.row;
+    return (
+        <tr key={row.id}>
+            <td>{row.id}</td>
+            <td>{row.email}</td>
+            <td>{row.firstName}</td>
+            <td>{row.lastName}</td>
+            <td>{row.registeredAt}</td>
+            <td><button className="btn btn-danger" data-letter={row.id} onClick={props.removeTableItem}>Delete</button></td>
+        </tr>
+    );
+
+}
+const TableBody = (props) => {
+    const rows = props.tableData.map((row, index) => {
+        return (
+            <TableRow key={index} row={row} removeTableItem={props.removeTableItem} />
+        )
+    })
+
+    return <tbody>{rows}</tbody>
+
+}
+
+
+export const Table = (props) => {
+
+    return (
+        <table className="table">
+            <TableHeader tableheaders={props.tableHeaders} />
+            <TableBody tableData={props.tableData} removeTableItem={props.removeTableItem} />
+        </table>
+    );
+}
+// Wrong
+//this.state.comment = 'Hello';
+
+// Correct
+//this.setState({comment: 'Hello'});
+
+export class AjaxTest extends React.Component {
+
+
+    //ctor
+    constructor(props) {
+
+        super(props);
+        this.state = {
+            posts: [],
+            comments: [],
+            users: []
+        };
+
+        this.endpoint = "https://rqjmvfm8l5.execute-api.eu-central-1.amazonaws.com/Prod/api/users";
+
+        //What on earth is the point of ES6 classes if their methods don't have proper lexical this binding,
+        //and then don't even expose a syntax for binding their context directly on their definition!?
+        this.getUsers = this.getUsers.bind(this);
+        this.deleteUser = this.deleteUser.bind(this);
+
+    }
+
+    AddUser() {
+
 
         const postBody = {
             email: "myreactemail@hexquote.com",
@@ -26,37 +100,77 @@ export class AjaxTest extends React.Component{
 
         //use browser's fetch API
         fetch(this.endpoint, requestData)
-        .then(res=> res.JSON())
-        .then(data => {
-            this.setState({
-                data
-            })
-        });
+            .then(res => res.JSON())
+            .then(data => {
+                this.setState({
+                    data
+                })
+            });
 
     }
 
-    getUsers(){
-        const endpoint = "https://rqjmvfm8l5.execute-api.eu-central-1.amazonaws.com/Prod/api/users";
+
+
+
+    componentDidMount22() {
+        fetchPosts().then(response => {
+            this.setState({
+                posts: response.posts
+            });
+        });
+
+        fetchComments().then(response => {
+            this.setState({
+                comments: response.comments
+            });
+        });
+    }
+
+    getUsers() {
+
         const requestData = {
             method: 'get',
             headers: {
                 'content-type': 'application/json'
-            }         
-        }
+            }
+        }      
 
-        fetch(endpoint, requestData)
-        .then(res=> res.json())
-        .then(data => {
-            console.log(data);
-
-            // this.setState({
-            //     data
-            // })
-        });
-
+        //ajax Call
+        fetch(this.endpoint, requestData)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                this.setState({
+                    users: data
+                })
+            });
     }
-    
 
+
+
+    deleteUser(e) {
+
+        let id = e.target.dataset.letter;
+        let deleteURL = `${this.endpoint}\\${id}`;
+        console.log(`DeleteUser`, deleteURL)
+
+        const requestData = {
+            method: 'delete',
+            headers: {
+                'content-type': 'application/json'
+            }
+        }
+        
+          //ajax Call
+          fetch(deleteURL, requestData)
+          .then(res => res)
+          .then(data => {
+              console.log(data);
+             this.getUsers(); //reload users
+          });
+
+        
+    }
 
     render() {
         return (
@@ -64,6 +178,8 @@ export class AjaxTest extends React.Component{
                 <h4>Ajax - FetchAPI</h4>
                 <button className="btn btn-primary" onClick={this.getUsers}>Get Users</button>
                 <button disabled onClick={this.AddUser}>Post</button>
+
+                <Table tableHeaders="id email firstName lastName registeredAt Actions" tableData={this.state.users} removeTableItem={this.deleteUser}></Table>
                 {/* <TodoList items={this.state.items} />
                 <form onSubmit={this.handleSubmit}>
                     <label htmlFor="new-todo">
