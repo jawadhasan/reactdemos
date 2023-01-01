c58d1c40407e10d0c091f5c5d82739cb2695b8ca
Jawad Shani <jawad.shani@hitachi-eu.com>
2023-01-01 16:31:14 +0100

    initial state for starMatch

--- src/star-match/index.js
+++ src/star-match/index.js
@@ -1,12 +1,20 @@
 import React, { useState } from "react";
 import utils from "./utils";
 
-const PlayNumer = ({number}) => {
+const StarsDisplay = props => (
+    <>
+        {utils.range(1, props.count).map(starId => (
+            <div key={starId} className="star" />
+        ))}
+    </>
+);
+
+const PlayNumer = ({ number }) => {
     return (
-        <button className="number">{number}</button>
-      );
+        <button className="number" onClick={() => console.log(number)}>{number}</button>
+    );
 }
- 
+
 
 const StarMatch = () => {
     const [stars, setStars] = useState(utils.random(1, 9));
@@ -15,9 +23,7 @@ const StarMatch = () => {
             <div className="help"> Pick one or more numbers that sum to the number of stars</div>
             <div className="body">
                 <div className="left">
-                    {utils.range(1, stars).map(starId =>
-                        <div key={starId} className="star" />
-                    )}
+                    <StarsDisplay count={stars} />
                 </div>
                 <div className="right">
                     {utils.range(1, 9).map(number =>
